e1a9135bdc699084eb57fc47fb3eaef28aa636e7
Alessandro <a.sparacio92@gmail.com>
2020-12-14 11:39:34 +0100

    Added histograms for order, frequency and type of source

--- SurveySam.R
+++ SurveySam.R
@@ -49,11 +49,31 @@ dati <- dati %>%
     LsourceString= Q28_5_TEXT
   )
 
-
 #remore worthless columns
 dati <- dati[,-4]
 #remore worthless rows
 dati <- dati[-c(1:2),]
+
+#summary and histogram for duration
+dati$Lduration <- as.numeric(dati$Lduration)
+summary(dati$Lduration)
+hist(dati$Lduration,main="duration",xlab="",ylab="")
+#Be aware: it won't work now because lduration must be fixed manually before 
+#running the script (e.g., weeks, must be converted in days)
+
+#summary and histogram for frequency 
+dati$Lfrequency <- as.numeric(dati$Lfrequency)
+summary(dati$Lfrequency)
+hist(dati$Lfrequency,main="frequency",xlab="",ylab="")
+
+#Is the order relevant?
+dati$Lorder <- as.character(dati$Lorder)
+table(dati$Lorder)
+
+#source
+dati$Lsource <- as.character(dati$Lsource)
+table(dati$Lsource)
+
 
 #select columns for name of exercises
 dati2 <-dati %>% select(Lexercise1:Lexercise10)
@@ -66,7 +86,10 @@ dati3 <- dati %>% select(Q41:Q41_10_TEXT)
 
 #Collapse into a single columns for protocol
 dati3 <-dati3 %>% gather(ID,Protocol,Q41,Q41_1_TEXT,Q41_2_TEXT,Q41_3_TEXT,Q41_4_TEXT,Q41_5_TEXT,Q41_6_TEXT,Q41_7_TEXT,Q41_8_TEXT,Q41_9_TEXT,Q41_10_TEXT)
+demoinfo <- cbind(dati$Q14, dati$Q16, dati$Q58)
+dati3 <-cbind(dati3, demoinfo)
 dati3 <- dati3[-c(1:7),] #remove some not useful rows
+dati3 <- dati3[,-43] #remore worthless columns
 
 #Separate by name,duration,frequency 
 dati3 <- separate(
@@ -76,6 +99,7 @@ dati3 <- separate(
   sep = ";"                       # where to separate
 ) 
 
+?colnames
 #############################################################
 # HIGH INTENSITY SELF-ADMINISTERED MINDFULNESS INTERVENTION #
 ###########################################################
@@ -102,6 +126,25 @@ dati <- dati %>%
     HsourceString= Q36_5_TEXT
   )
 
+#summary and histogram for duration
+dati$Hduration <- as.numeric(dati$Hduration)
+summary(dati$Hduration)
+hist(dati$Hduration,main="duration",xlab="",ylab="")
+#Be aware: it won't work now because lduration must be fixed manually before 
+#running the script (e.g., weeks, must be converted in days)
+
+#summary and histogram for frequency 
+dati$Hfrequency <- as.numeric(dati$Hfrequency)
+summary(dati$Hfrequency)
+hist(dati$Hfrequency,main="frequency",xlab="",ylab="")
+
+#Is the order relevant?
+dati$Lorder <- as.character(dati$Lorder)
+table(dati$Lorder)
+
+#source
+dati$Lsource <- as.character(dati$Lsource)
+table(dati$Lsource)
 
 #select columns for name of exercises
 dati4 <-dati %>% select(Hexercise1:Hexercise10)
@@ -114,6 +157,7 @@ dati5 <- dati %>% select(Q31:Q31_10_TEXT)
 
 #Collapse into a single columns for protocol
 dati5 <-dati5 %>% gather(ID,Protocol,Q31,Q31_1_TEXT,Q31_2_TEXT,Q31_3_TEXT,Q31_4_TEXT,Q31_5_TEXT,Q31_6_TEXT,Q31_7_TEXT,Q31_8_TEXT,Q31_9_TEXT,Q31_10_TEXT)
+dati5 <- cbind(dati5,demoinfo)
 dati5 <- dati5[-c(1:7),] #remove some not useful rows
 dati5 <- dati5[,-43] #remore worthless columns
 
@@ -124,3 +168,4 @@ dati5 <- separate(
   into = c("name", "minutes", "timeWeek", "numberWeek"), # new column names
   sep = ";"                       # where to separate
 ) 
+
